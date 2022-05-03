5407c6d1ac7e73f76ad3441d26af11a6b442c016
ajigeo <46032638+ajigeo@users.noreply.github.com>
2022-05-03 16:43:35 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,16 +1,6 @@
-import os
 import tensorflow as tf
-from keras import Model
-from keras.layers import Input
-from keras.layers import Dense
-from keras.layers import Flatten, BatchNormalization, SpatialDropout1D
-from keras.layers import LSTM
-from keras.layers.convolutional import Conv1D
-from keras.layers.pooling import MaxPooling1D
-from keras.layers.merge import concatenate
-#from keras.callbacks import EarlyStopping, ModelCheckpoint,CSVLogger
-from tensorflow.keras.optimizers import Adam
-from keras.utils.vis_utils import plot_model
+from tensorflow import keras
+from tensorflow.keras import layers
 
 from numpy.random import seed
 seed(100)
@@ -20,78 +10,68 @@ tf.config.run_functions_eagerly(True)
 #os.environ['PROJ_LIB'] = "D:/anaconda3/envs/gpu-enabled-spatial/Library/share/proj"
 physical_devices = tf.config.list_physical_devices("GPU")
 #print("Num GPUs Available: ", len(physical_devices))
-
-#%% Defined convLSTM block and the inception blocks for complex model
-def conv_lstm_module(input_layer,input_length,conv_filters,lstm_filters,kern1,kern2):    
-    b1 = Conv1D(conv_filters, kernel_size=kern1, activation='relu',padding='same')(input_layer)
-    b1 = Conv1D(conv_filters, kernel_size=kern1, activation='relu',padding='same')(b1)
-    b1 = Conv1D(conv_filters, kernel_size=kern1, activation='relu',padding='same')(b1)
-    b1 = BatchNormalization()(b1)
-    b2 = LSTM(lstm_filters,return_sequences=True)(input_layer)
-    b2 = LSTM(lstm_filters,return_sequences=True)(b2)
-    b2 = LSTM(lstm_filters,return_sequences=True)(b2)
-    b3 = LSTM(lstm_filters,return_sequences=True)(input_layer)
-    b3 = Conv1D(conv_filters, kernel_size=kern2, activation='relu',padding='same')(b3)
-    b3 = BatchNormalization()(b3)
-    b4 = MaxPooling1D(pool_size=3,padding='same',strides=1)(input_layer)
-    b4 = SpatialDropout1D(0.10)(b4)
-    concat1 = concatenate([b1,b2,b3,b4],axis=-1)
-    return concat1
-
-def inception_module(input_layer,conv_filters,kern1,kern2,kern3):
-    b5 = Conv1D(conv_filters, kernel_size=kern1, activation='relu',padding='same')(input_layer)
-    b6 = Conv1D(conv_filters, kernel_size=kern2, activation='relu',padding='same')(input_layer)
-    b7 = MaxPooling1D(pool_size=3,padding='same',strides=1)(input_layer)
-    b8 = Conv1D(conv_filters, kernel_size=kern3, activation='relu',padding='same')(input_layer)
-    concat2 = concatenate([b5,b6,b7,b8],axis=-1)
-    return concat2
-#%% Defined SAR+MS model
-def complex_fused_model():
-    vh_input = Input(shape=(8,1))
-    vh_conv_lstm = conv_lstm_module(vh_input,conv_filters=64,lstm_filters=64,kern1=3,kern2=3)
-    vh_flt_conv_lstm = Flatten()(vh_conv_lstm)
-    vh_inception = inception_module(vh_conv_lstm,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vh_inception = inception_module(vh_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vh_inception = inception_module(vh_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vh_inception = inception_module(vh_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vh_inception = inception_module(vh_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vh_inception = inception_module(vh_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vh_inception = inception_module(vh_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vh_flt_inception = Flatten()(vh_flt_conv_lstm)
-
-    vv_input = Input(shape=(8,1))
-    vv_conv_lstm = conv_lstm_module(vv_input,conv_filters=64,lstm_filters=64,kern1=3,kern2=3)
-    vv_flt_conv_lstm = Flatten()(vv_conv_lstm)
-    vv_inception = inception_module(vv_conv_lstm,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vv_inception = inception_module(vv_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vv_inception = inception_module(vv_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vv_inception = inception_module(vv_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vv_inception = inception_module(vv_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vv_inception = inception_module(vv_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vv_inception = inception_module(vv_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    vv_flt_inception = Flatten()(vv_inception)
-
-    mss_input = Input(shape=(12,1))
-    mss_conv_lstm = conv_lstm_module(mss_input,conv_filters=64,lstm_filters=64,kern1=3,kern2=3)
-    mss_flt_conv_lstm = Flatten()(mss_conv_lstm)
-    mss_inception = inception_module(mss_conv_lstm,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    mss_inception = inception_module(mss_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    mss_inception = inception_module(mss_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    mss_inception = inception_module(mss_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    mss_inception = inception_module(mss_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    mss_inception = inception_module(mss_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    mss_inception = inception_module(mss_inception,conv_filters=64,kern1=3,kern2=5,kern3=7)
-    mss_flt_inception = Flatten()(mss_inception)
-
-    concat_inputs = concatenate([vh_flt_conv_lstm,vv_flt_conv_lstm,mss_flt_conv_lstm,
-                                 vh_flt_inception,vv_flt_inception,mss_flt_inception])
-    hidden1 = Dense(64, activation='relu')(concat_inputs)
-    hidden2 = Dense(32, activation='relu')(hidden1)
-    final_output = Dense(13, activation='softmax')(hidden2)
-    final_model = Model(inputs=[vh_input,vv_input,mss_input],outputs=final_output)
-    adam_optimizer = Adam(learning_rate=0.0001)
-    final_model.compile(loss='sparse_categorical_crossentropy', optimizer=adam_optimizer, metrics=['accuracy'])
-    return final_model
 #%%
-my_fused_model = complex_fused_model()
-plot_model(my_fused_model, to_file='multiple_inputs.png', show_shapes=True,show_layer_names=False)
+def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout):
+    # Normalization and Attention
+    x = layers.MultiHeadAttention(
+        key_dim=head_size, num_heads=num_heads, dropout=dropout
+    )(inputs, inputs)
+    x = layers.Dropout(dropout)(x)
+    x = layers.LayerNormalization(epsilon=1e-6)(x)
+    res = x + inputs
+
+    # Feed Forward Part
+    x = layers.LayerNormalization(epsilon=1e-6)(res)
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
+    return x + res  
+#%%
+from keras.layers.merge import concatenate
+from keras import Model
+from tensorflow.keras.optimizers import Adam
+
+#n_classes = len(np.unique(y_train))
+def fusion_transformer(head_size, num_heads, ff_dim, num_transformer_blocks, mlp_units,dropout=0,mlp_dropout=0):
+    input1 = keras.Input(shape=(8,1))
+    x = input1
+    for _ in range(num_transformer_blocks):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+    x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
+    for dim in mlp_units:
+        x = layers.Dense(dim, activation="relu")(x)
+        x = layers.Dropout(mlp_dropout)(x)
+    output1 = layers.Dense(64, activation="relu")(x)
+    
+    input2 = keras.Input(shape=(8,1))
+    x = input2
+    for _ in range(num_transformer_blocks):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+    x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
+    for dim in mlp_units:
+        x = layers.Dense(dim, activation="relu")(x)
+        x = layers.Dropout(mlp_dropout)(x)
+    output2 = layers.Dense(64, activation="relu")(x)
+
+    input3 = keras.Input(shape=(12,1))
+    x = input3
+    for _ in range(num_transformer_blocks):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+    x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
+    for dim in mlp_units:
+        x = layers.Dense(dim, activation="relu")(x)
+        x = layers.Dropout(mlp_dropout)(x)
+    output3 = layers.Dense(64, activation="relu")(x)
+
+    final_concat = concatenate([output1,output2,output3],axis=-1)
+    hidden1 = layers.Dense(64, activation='relu')(final_concat)
+    hidden2 = layers.Dense(32, activation='relu')(hidden1)
+    hidden3 = layers.Dense(16, activation='relu')(hidden2)
+    final_output = layers.Dense(13, activation='softmax')(hidden3)
+    final_model = Model(inputs=[input1,input2,input3],outputs=final_output)
+    #adam_optimizer = Adam(learning_rate=0.0001)
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(0.0001,decay_steps=100000,decay_rate=0.96,staircase=True)
+    final_model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(learning_rate=lr_schedule), metrics=['accuracy'])
+    
+    return final_model
+    #return keras.Model(inputs, outputs)
